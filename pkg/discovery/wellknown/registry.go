ec56500791894ccd2702bc3e5ed00277d68dde9b
Patrick Pacher <patrick.pacher@gmail.com>
2024-10-27 15:09:47 +0100

    Change service catalog registry to be prefix based since consul does not accept multiple service on the same node:port instance

--- pkg/discovery/wellknown/registry.go
+++ pkg/discovery/wellknown/registry.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"math/rand/v2"
-	"os"
 
 	"github.com/bufbuild/connect-go"
 	"github.com/tierklinik-dobersberg/apis/gen/go/tkd/calendar/v1/calendarv1connect"
@@ -26,10 +25,6 @@ type Service[T any] struct {
 	Factory func(cli connect.HTTPClient, ep string, opts ...connect.ClientOption) T
 }
 
-func (s *Service[T]) Register(ctx context.Context, discoverer discovery.Discoverer, addr string, opts ...RegisterOption) error {
-	return Register(ctx, s, discoverer, addr, opts...)
-}
-
 func (s *Service[T]) Create(ctx context.Context, discoverer discovery.Discoverer, opts ...connect.ClientOption) (T, error) {
 	return NewClient(ctx, discoverer, *s, opts...)
 }
@@ -42,45 +37,58 @@ func Create[T any](name string, factory func(connect.HTTPClient, string, ...conn
 }
 
 var (
+	CalendarV1ServiceScope   = "tkd.calendar.v1"
+	CommentV1ServiceScope    = "tkd.comment.v1"
+	CustomerV1ServiceScope   = "tkd.customer.v1"
+	EventV1ServiceScropt     = "tkd.events.v1"
+	IdmV1ServiceScope        = "tkd.idm.v1"
+	OfficeHourV1ServiceScope = "tkd.office_hours.v1"
+	Pbx3cxV1ServiceScope     = "tkd.pbx3cx.v1"
+	RosterV1ServiceScope     = "tkd.roster.v1"
+	TaskV1ServiceScope       = "tkd.tasks.v1"
+	TypeV1ServiceScope       = "tkd.typeserver.v1"
+)
+
+var (
 	// tkd/calendar/v1
-	CalendarService = Create("tkd.calendar.v1.CalendarService", calendarv1connect.NewCalendarServiceClient)
-	HolidayService  = Create("tkd.calendar.v1.HolidayService", calendarv1connect.NewHolidayServiceClient)
+	CalendarService = Create(CalendarV1ServiceScope, calendarv1connect.NewCalendarServiceClient)
+	HolidayService  = Create(CalendarV1ServiceScope, calendarv1connect.NewHolidayServiceClient)
 
 	// tkd/comment/v1
-	CommentService = Create("tkd.comment.v1.CommentService", commentv1connect.NewCommentServiceClient)
+	CommentService = Create(CommentV1ServiceScope, commentv1connect.NewCommentServiceClient)
 
 	// tkd/customer/v1
-	CustomerService       = Create("tkd.customer.v1.CustomerService", customerv1connect.NewCustomerServiceClient)
-	CustomerImportService = Create("tkd.customer.v1.CustomerImportService", customerv1connect.NewCustomerImportServiceClient)
+	CustomerService       = Create(CustomerV1ServiceScope, customerv1connect.NewCustomerServiceClient)
+	CustomerImportService = Create(CustomerV1ServiceScope, customerv1connect.NewCustomerImportServiceClient)
 
 	// tkd/events/v1
-	EventService = Create("tkd.events.v1.EventService", eventsv1connect.NewEventServiceClient)
+	EventService = Create(EventV1ServiceScropt, eventsv1connect.NewEventServiceClient)
 
 	// tkd/idm/v1
-	UserService   = Create("tkd.idm.v1.UserService", idmv1connect.NewUserServiceClient)
-	RoleService   = Create("tkd.idm.v1.RoleService", idmv1connect.NewRoleServiceClient)
-	NotifyService = Create("tkd.idm.v1.NotifyService", idmv1connect.NewNotifyServiceClient)
-	AuthService   = Create("tkd.idm.v1.AuthService", idmv1connect.NewAuthServiceClient)
+	UserService   = Create(IdmV1ServiceScope, idmv1connect.NewUserServiceClient)
+	RoleService   = Create(IdmV1ServiceScope, idmv1connect.NewRoleServiceClient)
+	NotifyService = Create(IdmV1ServiceScope, idmv1connect.NewNotifyServiceClient)
+	AuthService   = Create(IdmV1ServiceScope, idmv1connect.NewAuthServiceClient)
 
 	// tkd/office_hours/v1
-	OfficeHoursService = Create("tkd.office_hours.v1.OfficeHourService", office_hoursv1connect.NewOfficeHourServiceClient)
+	OfficeHoursService = Create(OfficeHourV1ServiceScope, office_hoursv1connect.NewOfficeHourServiceClient)
 
 	// tkd/pbx3cx/v1
-	CallService      = Create("tkd.pbx3cx.v1.CallService", pbx3cxv1connect.NewCallServiceClient)
-	VoiceMailService = Create("tkd.pbx3cx.v1.VoiceMailService", pbx3cxv1connect.NewVoiceMailServiceClient)
+	CallService      = Create(Pbx3cxV1ServiceScope, pbx3cxv1connect.NewCallServiceClient)
+	VoiceMailService = Create(Pbx3cxV1ServiceScope, pbx3cxv1connect.NewVoiceMailServiceClient)
 
 	// tkd/roster/v1
-	RosterService    = Create("tkd.roster.v1.RosterService", rosterv1connect.NewRosterServiceClient)
-	WorkShiftService = Create("tkd.roster.v1.WorkShiftService", rosterv1connect.NewWorkShiftServiceClient)
-	WorkTimeService  = Create("tkd.roster.v1.WorkTimeService", rosterv1connect.NewWorkTimeServiceClient)
-	OffTimeService   = Create("tkd.roster.v1.OffTimeService", rosterv1connect.NewOffTimeServiceClient)
+	RosterService    = Create(RosterV1ServiceScope, rosterv1connect.NewRosterServiceClient)
+	WorkShiftService = Create(RosterV1ServiceScope, rosterv1connect.NewWorkShiftServiceClient)
+	WorkTimeService  = Create(RosterV1ServiceScope, rosterv1connect.NewWorkTimeServiceClient)
+	OffTimeService   = Create(RosterV1ServiceScope, rosterv1connect.NewOffTimeServiceClient)
 
 	// tkd/tasks/v1
-	BoardService = Create("tkd.tasks.v1.BoardService", tasksv1connect.NewBoardServiceClient)
-	TaskService  = Create("tkd.tasks.v1.TaskService", tasksv1connect.NewTaskServiceClient)
+	BoardService = Create(TaskV1ServiceScope, tasksv1connect.NewBoardServiceClient)
+	TaskService  = Create(TaskV1ServiceScope, tasksv1connect.NewTaskServiceClient)
 
 	// tkd/typeserver/v1
-	TypeService = Create("tkd.typeserver.v1.TypeResolverService", typeserverv1connect.NewTypeResolverServiceClient)
+	TypeService = Create(TypeV1ServiceScope, typeserverv1connect.NewTypeResolverServiceClient)
 )
 
 func NewClient[T any](ctx context.Context, d discovery.Discoverer, wks Service[T], opts ...connect.ClientOption) (T, error) {
@@ -98,44 +106,4 @@ func NewClient[T any](ctx context.Context, d discovery.Discoverer, wks Service[T
 	i := svc[rand.IntN(len(svc))]
 
 	return wks.Factory(cli.NewInsecureHttp2Client(), i.Address, opts...), nil
-}
-
-type RegisterOption func(instance *discovery.ServiceInstance)
-
-func Register[T any](ctx context.Context, wk *Service[T], discoverer discovery.Discoverer, addr string, opts ...RegisterOption) error {
-	instance := &discovery.ServiceInstance{
-		Name:    wk.Name,
-		Address: addr,
-	}
-
-	for _, opt := range opts {
-		opt(instance)
-	}
-
-	if instance.Instance == "" {
-		if i := os.Getenv("INSTANCE_ID"); i != "" {
-			instance.Instance = i
-		} else {
-			hostname, err := os.Hostname()
-			if err != nil {
-				return fmt.Errorf("failed to get hostname: %w", err)
-			}
-
-			instance.Instance = hostname
-		}
-	}
-
-	return discovery.Register(ctx, discoverer, *instance)
-}
-
-func WithInstanceID(id string) RegisterOption {
-	return func(instance *discovery.ServiceInstance) {
-		instance.Instance = id
-	}
-}
-
-func WithMeta(meta map[string]string) RegisterOption {
-	return func(instance *discovery.ServiceInstance) {
-		instance.Meta = meta
-	}
 }
